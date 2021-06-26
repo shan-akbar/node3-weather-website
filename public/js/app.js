df7b97a619573620c567c4bcb168a88aa097bac6
Shan Akbar <shanakbarsanga@gmail.com>
2021-06-26 17:22:48 +0500

    =Added some extra description about weather.

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,14 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
+const messageThree = document.querySelector('#message-3')
+const messageFour = document.querySelector('#message-4')
+const messageFive = document.querySelector('#message-5')
+const messageSix = document.querySelector('#message-6')
+const messageSeven = document.querySelector('#message-7')
+const messageEight = document.querySelector('#message-8')
+const messageNine = document.querySelector('#message-9')
+const messageTen = document.querySelector('#message-10')
 
 weatherForm.addEventListener('submit', (e) =>{
     e.preventDefault()
@@ -18,12 +25,15 @@ weatherForm.addEventListener('submit', (e) =>{
         } else {
             messageOne.textContent = data.Location
             messageTwo.textContent = data.Forecast.weather_desc + ', It is ' + data.Forecast.temp + 
-            ' degrees out there, but it feels like ' + data.Forecast.feels_like_temp + ' degrees.'+'<br>'+
-            'Observation time: '+data.Forecast.observation_time+'<br>'+
-            'Wind speed: '+data.Forecast.wind_speed+'<br>'+
-            'Wind direction: '+data.Forecast.wind_dir+'<br>'+
-            'Cloud cover: '+data.Forecast.cloudcover+'<br>'+
-            'Day time: '+data.Forecast.is_day
+            ' degrees out there, but it feels like ' + data.Forecast.feels_like_temp + ' degrees.'
+            messageThree.textContent = 'Observation time: '+data.Forecast.observation_time
+            messageFour.textContent = 'Wind speed: '+data.Forecast.wind_speed+' km/h'
+            messageFive.textContent = 'Wind pressure: '+data.Forecast.pressure+ ' milibar' 
+            messageSix.textContent = 'Wind diection: '+data.Forecast.wind_direction
+            messageSeven.textContent = 'Humidity: '+data.Forecast.humidity+ '%'
+            messageEight.textContent = 'Cloud cover: '+data.Forecast.cloudcover+ '%'
+            messageNine.textContent = 'Visibility range: '+data.Forecast.visibility+' km'
+            messageTen.textContent = 'Day time: '+data.Forecast.is_day
         }
     })
 })
