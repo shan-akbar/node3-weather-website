ee1fa62e91d7d01903cb70ac924d43cc5706c424
Shan Akbar <shanakbarsanga@gmail.com>
2021-06-26 15:12:11 +0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener('submit', (e) =>{
     const location = search.value
 
     messageOne.textContent = 'Loading...'
-    const url = 'http://localhost:3000/weather?search=' + location
+    const url = '/weather?search=' + location
     fetch(url).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
